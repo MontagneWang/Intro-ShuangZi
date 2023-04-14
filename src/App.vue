6d72fa18aae7d4f624830e85631d37d6e3acbabf
山遥路远 <2214981880@qq.com>
2023-04-14 22:46:32 +0800

    提交

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+	<h1>这里以后会成为 <a href="https://space.bilibili.com/193181849" target="_blank">起氏双子</a> 的介绍页面</h1>
+	<div>不过还在努力开发中……</div>
+	<br />
+	<div>如果您是官方且需要回收该域名，可通过 <a href="mailto:ling.vc@foxmail.com" target="_blank">邮箱</a> 联系到我，我会将域名转入到您所提供的域名注册商</div>
+	<br />
+	<div>如果您是纯路人，不妨听听以下几首音乐</div>
+</template>
+
+<style scoped>
+html *
+{
+	font-family: LXGW WenKai Lite, serif !important;
+}
+
+</style>
