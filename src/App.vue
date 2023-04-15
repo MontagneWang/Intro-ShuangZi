5dbfdea07495d88d48f3fa265fde0cdb93d37eba
山遥路远 <2214981880@qq.com>
2023-04-15 12:57:55 +0800

    完成第一页开发

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
 		<h2>不过还在努力开发中……</h2>
 		<br/>
 		<div>如果您是官方且需要回收该域名，可以通过 <a href="mailto:ling.vc@foxmail.com" target="_blank" title="邮箱：ling.vc@foxmail.com">邮箱</a>
-			联系到我，我会将当前域名转入到您所提供的域名注册商
+			联系我，我会将当前域名转入到您所提供的域名注册商
 		</div>
 		<br/>
 		<div>如果您是双子的听众，我想请您先保密，等页面开发完再来坐坐</div>
@@ -26,11 +26,7 @@
 		<iframe border="0" frameborder="no" height=100
 		        marginheight="0" marginwidth="0" src="//music.163.com/outchain/player?type=2&id=1957143939&auto=0&height=66"
 		        width=430></iframe>
-		<div class="time">页面构建日期：2023-04-14</div>
-		<div class="time">最后修改日期：2023-04-15</div>
-		<div class="time">
-			<del>请给我一点点时间，也请降低期待</del>
-		</div>
+		<div class="time">页面开始构建日期：2023-04-14</div>
 	</div>
 </template>
 
