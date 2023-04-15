4a0a95170d3c56db43334c22842d58f5b1aea980
山遥路远 <2214981880@qq.com>
2023-04-15 23:56:22 +0800

    完成第一页开发

--- src/App.vue
+++ src/App.vue
@@ -3,36 +3,7 @@ import IntroPage from "./components/IntroPage.vue";
 </script>
 
 <template>
-<<<<<<< HEAD
 	<intro-page/>
-=======
-	<div class="bg">
-		<h1>
-			你好！这里是
-			<a href="https://space.bilibili.com/193181849"
-			   target="_blank">
-				<img alt="起氏双子"
-				     src="./assets/Logo.png"
-				     title="点击前往 起氏双子 官方">
-			</a>
-			的介绍页面
-		</h1>
-		<h2>不过还在努力开发中……</h2>
-		<br/>
-		<div>如果您是官方且需要回收该域名，可以通过 <a href="mailto:ling.vc@foxmail.com" target="_blank" title="邮箱：ling.vc@foxmail.com">邮箱</a>
-			联系我，我会将当前域名转入到您所提供的域名注册商
-		</div>
-		<br/>
-		<div>如果您是双子的听众，我想请您先保密，等页面开发完再来坐坐</div>
-		<br/>
-		<div>如果您是纯路人，我想请您听听这首音乐</div>
-		<br/>
-		<iframe border="0" frameborder="no" height=100
-		        marginheight="0" marginwidth="0" src="//music.163.com/outchain/player?type=2&id=1957143939&auto=0&height=66"
-		        width=430></iframe>
-		<div class="time">页面开始构建日期：2023-04-14</div>
-	</div>
->>>>>>> origin/main
 </template>
 
 <style lang="scss" scoped>
