9e722dc2ca4e74bd8f98f1c2226071091e5ddd44
山遥路远 <2214981880@qq.com>
2023-04-15 12:07:21 +0800

    提交初版

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -10,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
 			   target="_blank">
 				<img alt="起氏双子"
 				     src="./assets/Logo.png"
-				     title="assets/Logo.png">
+				     title="点击前往 起氏双子 官方">
 			</a>
 			的介绍页面
 		</h1>
@@ -20,12 +19,18 @@ import HelloWorld from './components/HelloWorld.vue'
 			联系到我，我会将当前域名转入到您所提供的域名注册商
 		</div>
 		<br/>
+		<div>如果您是双子的听众，我想请您先保密，等页面开发完再来坐坐</div>
+		<br/>
 		<div>如果您是纯路人，我想请您听听这首音乐</div>
 		<br/>
 		<iframe border="0" frameborder="no" height=100
 		        marginheight="0" marginwidth="0" src="//music.163.com/outchain/player?type=2&id=1957143939&auto=0&height=66"
 		        width=430></iframe>
-		<div class="time">页面最后修改日期：2023-04-15</div>
+		<div class="time">页面构建日期：2023-04-14</div>
+		<div class="time">最后修改日期：2023-04-15</div>
+		<div class="time">
+			<del>请给我一点点时间，也请降低期待</del>
+		</div>
 	</div>
 </template>
 
